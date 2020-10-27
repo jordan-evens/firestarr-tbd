44f9ce4e927689017dd37f1156841438c68dfd16
Jordan Evens <jordan.evens@gmail.com>
2020-10-27 15:35:12 -0400

    removed test data that relied on specific fuels and fire data

--- FireSTARR/run.bat
+++ FireSTARR/run.bat
@@ -9,10 +9,10 @@ call "%p%"
 :build
 @rem clear this because it causes changes to where project builds and other things
 SET Platform=
-SET ARGS=-ArgumentList '.\Data\output 2017-08-27 52.01 -89.024 12:15'
+SET ARGS=
 IF "%*" NEQ "" SET ARGS=-ArgumentList '%*'
-@rem ~ msbuild /p:Configuration=Release /t:Clean,Build /m /warnaserror || (set ERRORLEVEL=1 && goto :end)
 msbuild /p:Configuration=Release /t:Build /m /warnaserror || (set ERRORLEVEL=1 && goto :end)
-rm -rf Data\output || goto :end
-mkdir Data\output || goto :end
 PowerShell Measure-Command {start-process Release\firestarr.exe %ARGS% -Wait -NoNewWindow}
+
+:end
+%COMSPEC% /C exit %ERRORLEVEL% >nul
