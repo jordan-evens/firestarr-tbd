8a3c365713c70c5ac93db28f215147d3ed0b3dcb
Jordan Evens <jordan.evens@gmail.com>
2020-12-23 13:12:24 -0500

    theoretically, removing water and adding from polygons should work, but nodata values will not be filled yet

--- GIS/grids/make_grids.py
+++ GIS/grids/make_grids.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import gc
 #import shapefile
 import rasterio
@@ -19,6 +20,7 @@ import numpy as np
 import math
 import pandas as pd
 import gdalconst
+from osgeo import ogr
 
 from Settings import Settings
 
@@ -99,10 +101,132 @@ def clip_zone(fp, prefix, zone):
     rb = ds.GetRasterBand(1)
     # HACK: for some reason no_data is a double??
     if no_data is None:
-        rb.SetNoDataValue(int(-math.pow(2, 15) - 1))
+        no_data = int(-math.pow(2, 15) - 1)
+        rb.SetNoDataValue(no_data)
     rb = None
     ds = None
     gc.collect()
+    return out_tif
+
+def clip_fuel(fp, prefix, zone):
+    # fp = os.path.join(EXTRACTED_DIR, r'fbp\fuel_layer\FBP_FuelLayer.tif')
+    # prefix = 'fuel'
+    # zone = 15.0
+    out_tif = os.path.join(DIR, prefix + '_{}'.format(zone).replace('.', '_')) + '.tif'
+    if os.path.exists(out_tif):
+        return out_tif
+    print(out_tif)
+    meridian = (zone - 15.0) * 6.0 - 93.0
+    wkt = 'PROJCS["NAD_1983_UTM_Zone_{}N",GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["False_Easting",500000.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",{}],PARAMETER["Scale_Factor",0.9996],PARAMETER["Latitude_Of_Origin",0.0],UNIT["Meter",1.0]]'.format(zone, meridian)
+    proj_srs = osr.SpatialReference(wkt=wkt)
+    toProj = Proj(proj_srs.ExportToProj4())
+    lat = (meridian, meridian)
+    lon = (settings.latitude_min, settings.latitude_max)
+    df = pd.DataFrame(np.c_[lat, lon], columns=['Longitude', 'Latitude'])
+    x, y = toProj(df['Longitude'].values, df['Latitude'].values)
+    MIN_EASTING = 300000
+    MAX_EASTING = 700000
+    MIN_NORTHING = int(y[0] / 100000) * 100000
+    MAX_NORTHING = (int(y[1] / 100000) + 1) * 100000
+    ds = gdal.Open(fp)
+    out_image = None
+    out_transform = None
+    data = rasterio.open(fp)
+    srcWkt = data.crs.wkt
+    data.close()
+    srcSRS = osr.SpatialReference()
+    srcSRS.ImportFromWkt(data.crs.wkt)
+    dstSRS = osr.SpatialReference()
+    dstSRS.ImportFromWkt(wkt)
+    rb = ds.GetRasterBand(1)
+    no_data = rb.GetNoDataValue()
+    rb = None
+    ds = gdal.Warp(out_tif,
+                   ds,
+                   format='GTiff',
+                   outputBounds=[MIN_EASTING, MIN_NORTHING, MAX_EASTING, MAX_NORTHING],
+                   creationOptions=CREATION_OPTIONS,
+                   xRes=CELL_SIZE,
+                   yRes=CELL_SIZE,
+                   srcSRS=srcWkt,
+                   dstSRS=wkt)
+    ds = None
+    # HACK: make sure nodata value is set because C code expects it even if nothing is nodata
+    ds = gdal.Open(out_tif, 1)
+    rb = ds.GetRasterBand(1)
+    # HACK: for some reason no_data is a double??
+    if no_data is None:
+        no_data = int(-math.pow(2, 15) - 1)
+        rb.SetNoDataValue(no_data)
+    print('Removing water')
+    rows = ds.RasterYSize
+    cols = ds.RasterXSize
+    vals = rb.ReadAsArray(0, 0, cols, rows)
+    # get rid of water (102)
+    vals[vals == 102] = no_data
+    rb.WriteArray(vals, 0, 0)
+    rb.FlushCache()
+    rb = None
+    ds = None
+    gc.collect()
+    gdb_driver = ogr.GetDriverByName("OpenFileGDB")
+    print('Adding water from polygons')
+    # FIX: need to add USA water
+    # FIX: check bounds for provinces and don't add if not near raster
+    for prov in ['AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT']:
+        print('Adding {}'.format(prov))
+        path_gdb = r'C:\FireGUARD\data\extracted\canvec\canvec_50K_{}_Hydro.gdb'.format(prov)
+        gdb = gdb_driver.Open(path_gdb, 0)
+        lakes = gdb.GetLayer('waterbody_2')
+        ds = gdal.Open(out_tif, 1)
+        transform = ds.GetGeoTransform()
+        pixelWidth = transform[1]
+        pixelHeight = transform[5]
+        cols = ds.RasterXSize
+        rows = ds.RasterYSize
+        xLeft = transform[0]
+        yTop = transform[3]
+        xRight = xLeft+cols*pixelWidth
+        yBottom = yTop+rows*pixelHeight
+
+        raster_ref = osr.SpatialReference(wkt=ds.GetProjection())
+        lakes_ref = lakes.GetSpatialRef()
+        coordTrans = osr.CoordinateTransformation(raster_ref, lakes_ref)
+        ring = ogr.Geometry(ogr.wkbLinearRing)
+        ring.AddPoint(xLeft, yTop)
+        ring.AddPoint(xLeft, yBottom)
+        ring.AddPoint(xRight, yBottom)
+        ring.AddPoint(xRight, yTop)
+        ring.AddPoint(xLeft, yTop)
+        rasterGeometry = ogr.Geometry(ogr.wkbPolygon)
+        rasterGeometry.AddGeometry(ring)
+        rasterGeometry.AssignSpatialReference(raster_ref)
+        rasterGeometry.Transform(coordTrans)
+        e1 = rasterGeometry.GetEnvelope()
+        e2 = lakes.GetExtent()
+        r = ogr.Geometry(ogr.wkbLinearRing)
+        r.AddPoint(e2[0], e2[3])
+        r.AddPoint(e2[0], e2[2])
+        r.AddPoint(e2[1], e2[2])
+        r.AddPoint(e2[1], e2[3])
+        r.AddPoint(e2[0], e2[3])
+        vectorGeometry = ogr.Geometry(ogr.wkbPolygon)
+        vectorGeometry.AddGeometry(r)
+        vectorGeometry.AssignSpatialReference(lakes_ref)
+        # FIX: just check full extent once and not every feature
+        # intersects = False
+        # for f in lakes:
+            # vectorGeometry = f.GetGeometryRef()
+            # intersects = rasterGeometry.Intersect(vectorGeometry)
+            # if intersects:
+                # break
+        # just expecting RasterizeLayer to handle projection and everything
+        # just do basic math for now
+        # if (e1[0] >= e2[0] and e1[0] <= e2[1])
+        if vectorGeometry.Intersect(rasterGeometry):
+            print('Province {} intersects zone'.format(prov))
+            gdal.RasterizeLayer(ds, [1], lakes, burn_values=[102])
+    # now need to fill in the nodata values that are left
     return out_tif
 
 zone = ZONE_MIN
@@ -120,5 +244,5 @@ while zone <= ZONE_MAX:
         tmp_aspect = aspect.replace(DIR, TMP)
         gdal.DEMProcessing(tmp_aspect, dem, 'aspect', creationOptions=CREATION_OPTIONS)
         gdal.Translate(aspect, tmp_aspect, outputType=gdalconst.GDT_UInt16, creationOptions=CREATION_OPTIONS)
-    fbp = clip_zone(os.path.join(EXTRACTED_DIR, r'fbp\fuel_layer\FBP_FuelLayer.tif'), 'fuel', zone)
+    fbp = clip_fuel(os.path.join(EXTRACTED_DIR, r'fbp\fuel_layer\FBP_FuelLayer.tif'), 'fuel', zone)
     zone += 0.5
