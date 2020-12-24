985fe27d16c7cca35dbfcc29879d48c79066f0f3
Jordan Evens <jordan.evens@gmail.com>
2020-12-24 09:04:04 -0500

    added downloading nhd data

--- GIS/grids/collect.py
+++ GIS/grids/collect.py
@@ -12,10 +12,12 @@ DATA_DIR = os.path.realpath('/FireGUARD/data')
 EXTRACTED_DIR = os.path.join(DATA_DIR, 'extracted')
 DOWNLOAD_DIR = os.path.join(DATA_DIR, 'download')
 canada = r'http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/lcsd000a19a_e.zip'
-
 fbp = r'https://cwfis.cfs.nrcan.gc.ca/downloads/fuels/development/Canadian_Forest_FBP_Fuel_Types/Canadian_Forest_FBP_Fuel_Types_v20191114.zip'
+nhd = r'https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/NHD/National/HighResolution/GDB/NHD_H_National_GDB.zip'
+
 if not os.path.exists(EXTRACTED_DIR):
     os.makedirs(EXTRACTED_DIR)
 unzip(download(canada, DOWNLOAD_DIR), os.path.join(EXTRACTED_DIR, r'canada'))
 unzip(download(fbp, DOWNLOAD_DIR), os.path.join(EXTRACTED_DIR, r'fbp'))
+unzip(download(nhd, DOWNLOAD_DIR), os.path.join(EXTRACTED_DIR, r'nhd'))
 
