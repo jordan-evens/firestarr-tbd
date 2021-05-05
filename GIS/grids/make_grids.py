8de38dea2dbde5df4a22533eee8f4d02d8fb5306
Jordan Evens <jordan.evens@gmail.com>
2021-05-05 10:09:21 -0400

    moved code into check_base()

--- GIS/grids/make_grids.py
+++ GIS/grids/make_grids.py
@@ -280,6 +280,63 @@ def checkAddLakes(zone, cols, rows, for_what, path_gdb, layer):
         return outputRaster
     return None
 
+def check_base(fp, zone):
+    base_tif = os.path.join(INT_FUEL, 'base_{}'.format(zone).replace('.', '_')) + '.tif'
+    if not os.path.exists(base_tif):
+        meridian = (zone - 15.0) * 6.0 - 93.0
+        wkt = 'PROJCS["NAD_1983_UTM_Zone_{}N",GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["False_Easting",500000.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",{}],PARAMETER["Scale_Factor",0.9996],PARAMETER["Latitude_Of_Origin",0.0],UNIT["Meter",1.0]]'.format(zone, meridian)
+        proj_srs = osr.SpatialReference(wkt=wkt)
+        toProj = Proj(proj_srs.ExportToProj4())
+        lat = (meridian, meridian)
+        lon = (settings.latitude_min, settings.latitude_max)
+        df = pd.DataFrame(np.c_[lat, lon], columns=['Longitude', 'Latitude'])
+        x, y = toProj(df['Longitude'].values, df['Latitude'].values)
+        MIN_EASTING = 300000
+        MAX_EASTING = 700000
+        MIN_NORTHING = int(y[0] / 100000) * 100000
+        MAX_NORTHING = (int(y[1] / 100000) + 1) * 100000
+        ds = gdal.Open(fp)
+        out_image = None
+        out_transform = None
+        data = rasterio.open(fp)
+        srcWkt = data.crs.wkt
+        data.close()
+        srcSRS = osr.SpatialReference()
+        srcSRS.ImportFromWkt(data.crs.wkt)
+        dstSRS = osr.SpatialReference()
+        dstSRS.ImportFromWkt(wkt)
+        rb = ds.GetRasterBand(1)
+        no_data = rb.GetNoDataValue()
+        rb = None
+        ds = gdal.Warp(base_tif,
+                       ds,
+                       format='GTiff',
+                       outputBounds=[MIN_EASTING, MIN_NORTHING, MAX_EASTING, MAX_NORTHING],
+                       creationOptions=CREATION_OPTIONS,
+                       xRes=CELL_SIZE,
+                       yRes=CELL_SIZE,
+                       srcSRS=srcWkt,
+                       dstSRS=wkt)
+        ds = None
+        # HACK: make sure nodata value is set because C code expects it even if nothing is nodata
+        ds = gdal.Open(base_tif, 1)
+        rb = ds.GetRasterBand(1)
+        # HACK: for some reason no_data is a double??
+        if no_data is None:
+            no_data = int(-math.pow(2, 15) - 1)
+            rb.SetNoDataValue(no_data)
+            rb.FlushCache()
+        rb = None
+        ds = None
+    ds = gdal.Open(base_tif, 1)
+    rows = ds.RasterYSize
+    cols = ds.RasterXSize
+    rb = ds.GetRasterBand(1)
+    no_data = rb.GetNoDataValue()
+    rb = None
+    ds = None
+    return base_tif, cols, rows, no_data
+
 def check_nowater(base_tif, zone, cols, rows, no_data):
     nowater_tif = os.path.join(INT_FUEL, 'nowater_{}'.format(zone).replace('.', '_')) + '.tif'
     if not os.path.exists(nowater_tif):
@@ -379,62 +436,9 @@ def clip_fuel(fp, zone):
     if os.path.exists(out_tif):
         return out_tif
     print(out_tif)
-    base_tif = os.path.join(INT_FUEL, 'base_{}'.format(zone).replace('.', '_')) + '.tif'
     polywater_tif = os.path.join(INT_FUEL, 'polywater_{}'.format(zone).replace('.', '_')) + '.tif'
     merged_tif = os.path.join(INT_FUEL, 'merged_{}'.format(zone).replace('.', '_')) + '.tif'
-    meridian = (zone - 15.0) * 6.0 - 93.0
-    wkt = 'PROJCS["NAD_1983_UTM_Zone_{}N",GEOGCS["GCS_North_American_1983",DATUM["D_North_American_1983",SPHEROID["GRS_1980",6378137.0,298.257222101]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["False_Easting",500000.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",{}],PARAMETER["Scale_Factor",0.9996],PARAMETER["Latitude_Of_Origin",0.0],UNIT["Meter",1.0]]'.format(zone, meridian)
-    proj_srs = osr.SpatialReference(wkt=wkt)
-    toProj = Proj(proj_srs.ExportToProj4())
-    lat = (meridian, meridian)
-    lon = (settings.latitude_min, settings.latitude_max)
-    df = pd.DataFrame(np.c_[lat, lon], columns=['Longitude', 'Latitude'])
-    x, y = toProj(df['Longitude'].values, df['Latitude'].values)
-    MIN_EASTING = 300000
-    MAX_EASTING = 700000
-    MIN_NORTHING = int(y[0] / 100000) * 100000
-    MAX_NORTHING = (int(y[1] / 100000) + 1) * 100000
-    if not os.path.exists(base_tif):
-        ds = gdal.Open(fp)
-        out_image = None
-        out_transform = None
-        data = rasterio.open(fp)
-        srcWkt = data.crs.wkt
-        data.close()
-        srcSRS = osr.SpatialReference()
-        srcSRS.ImportFromWkt(data.crs.wkt)
-        dstSRS = osr.SpatialReference()
-        dstSRS.ImportFromWkt(wkt)
-        rb = ds.GetRasterBand(1)
-        no_data = rb.GetNoDataValue()
-        rb = None
-        ds = gdal.Warp(base_tif,
-                       ds,
-                       format='GTiff',
-                       outputBounds=[MIN_EASTING, MIN_NORTHING, MAX_EASTING, MAX_NORTHING],
-                       creationOptions=CREATION_OPTIONS,
-                       xRes=CELL_SIZE,
-                       yRes=CELL_SIZE,
-                       srcSRS=srcWkt,
-                       dstSRS=wkt)
-        ds = None
-        # HACK: make sure nodata value is set because C code expects it even if nothing is nodata
-        ds = gdal.Open(base_tif, 1)
-        rb = ds.GetRasterBand(1)
-        # HACK: for some reason no_data is a double??
-        if no_data is None:
-            no_data = int(-math.pow(2, 15) - 1)
-            rb.SetNoDataValue(no_data)
-            rb.FlushCache()
-        rb = None
-        ds = None
-    ds = gdal.Open(base_tif, 1)
-    rows = ds.RasterYSize
-    cols = ds.RasterXSize
-    rb = ds.GetRasterBand(1)
-    no_data = rb.GetNoDataValue()
-    # close even though we're opening it in if statement so we know it's closed if we don't enter if
-    ds = None
+    base_tif, cols, rows, no_data = check_base(fp, zone)
     nowater_tif = check_nowater(base_tif, zone, cols, rows, no_data)
     filled_tif = check_filled(base_tif, nowater_tif, zone, cols, rows, no_data)
     # now the nodata values should all be filled, so apply the water from the polygons
