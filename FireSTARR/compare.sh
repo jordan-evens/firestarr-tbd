bcf6ab7a544a4461822cf6d34b5df1ea20d21f4b
Jordan Evens <jordan.evens@gmail.com>
2022-02-11 09:22:15 -0500

    added script to help compare debug and release version output

---
+++ FireSTARR/compare.sh
@@ -0,0 +1,2 @@
+cmake-build-debug/FireSTARR ./Data/output.debug 2017-08-27 52.01 -89.024 12:15 -i -v -v --wx test/wx.csv --ffmc 90 --dmc 40 --dc 300 --apcp_0800 0
+cmake-build-release/FireSTARR ./Data/output.release 2017-08-27 52.01 -89.024 12:15 -i -v -v --wx test/wx.csv --ffmc 90 --dmc 40 --dc 300 --apcp_0800 0
