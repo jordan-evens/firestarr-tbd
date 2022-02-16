918e255ee4f38755328eb0c1cba7137a83d5a2e5
Jordan Evens <jordan.evens@gmail.com>
2022-02-16 07:54:24 -0500

    added making build files to compare.sh

--- FireSTARR/compare.sh
+++ FireSTARR/compare.sh
@@ -1,3 +1,5 @@
+cmake -S . -B cmake-build-debug -D CMAKE_BUILD_TYPE=Debug
+cmake -S . -B cmake-build-release -D CMAKE_BUILD_TYPE=Release
 cmake --build cmake-build-debug -j 48
 cmake --build cmake-build-release -j 48
 cmake-build-debug/FireSTARR ./Data/output.debug 2017-08-27 52.01 -89.024 12:15 -i -v -v --wx test/wx.csv --ffmc 90 --dmc 40 --dc 300 --apcp_0800 0
