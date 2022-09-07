a66dea0694b45d0e8b6eccd31f5b4744090eef09
Jordan Evens <jordan.evens@gmail.com>
2022-09-07 13:58:28 -0400

    added test script

---
+++ tbd/test_hourly.sh
@@ -0,0 +1,2 @@
+export ARGS="2017-08-27 52.01 -89.024 12:15 $* --wx test/wx_hourly_in.csv --ffmc 90 --dmc 40 --dc 300 --apcp_0800 0"
+cmake -S . -B cmake-build-release -D CMAKE_BUILD_TYPE=Release && cmake --build cmake-build-release -j 48 && time cmake-build-release/tbd ./data/output.hourly ${ARGS}
