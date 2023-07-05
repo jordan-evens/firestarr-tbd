702bf6449ac19d5f730edd991acaae1cbb9c9325
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-07-05 14:43:46 -0400

    clean up update.sh

--- tbd/update.sh
+++ tbd/update.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 URL_TEST="https://spotwx.com/products/grib_index.php?model=geps_0p5_raw&lat=48.80686&lon=-87.45117&tz=-5&label="
 DIR=/appl/data
+CURDATE=`date -u --rfc-3339=seconds`
 PREFIX=geps
 FILE_LATEST=${DIR}/${PREFIX}_latest
 FILE_CURRENT=${DIR}/${PREFIX}_current
 FILE_TMP=${DIR}/${PREFIX}_tmp
-echo Running update at `date`
+FILE_LOG=${DIR}/${PREFIX}_log
 source /appl/.venv/bin/activate
 cd /appl/tbd
+# echo ${CURDATE} >> ${FILE_LOG}
 # copy after trying instead of going right to ${FILE_LATEST} in case curl fails and makes an empty file
-((curl -sk "${URL_TEST}" | grep "Model date" > ${FILE_TMP}) \
+( \
+    ( \
+        (curl -sk "${URL_TEST}" | grep "Model date" > ${FILE_TMP}) \
         && (mv ${FILE_TMP} ${FILE_LATEST}) \
-        && ((diff ${FILE_CURRENT} ${FILE_LATEST} \
-                && echo Model already matches `cat ${FILE_LATEST}`) \
-            || ((python main.py) \
-                && (cp ${FILE_LATEST} ${FILE_CURRENT}) \
-                && (./publish_geoserver.sh)))) \
-    || (echo Run attempt failed)
+    ) \
+    || ( \
+        (echo ${CURDATE}: failed to get ${URL_TEST} | tee -a ${FILE_LOG}) \
+        && (exit -1) \
+    ) \
+) \
+&& ( \
+    ( \
+        diff ${FILE_CURRENT} ${FILE_LATEST} \
+        && echo ${CURDATE}: Already up to date >> ${FILE_LOG}
+    ) \
+    || \
+    ( \
+        (echo ${CURDATE}: Running update  | tee -a ${FILE_LOG}) \
+        && (python main.py) \
+        && (cp ${FILE_LATEST} ${FILE_CURRENT}) \
+        && (./publish_geoserver.sh) \
+        && (echo ${CURDATE}: Done update  | tee -a ${FILE_LOG}) \
+    ) \
+) \
+|| (echo ${CURDATE}: Run attempt failed | tee -a ${FILE_LOG})
