8a19f057fdf2e1460e679ce9d6d83f2467d6a975
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-07-08 12:39:39 -0400

    move geoserver credentials into geoserver_setup.sh

--- tbd/publish_geoserver.sh
+++ tbd/publish_geoserver.sh
@@ -1,23 +1,31 @@
 #/bin/bash
-# LAYER=firestarr_mount
-# COVERAGE=FireSTARR
-# EXTENSION=imagemosaic
-# CREDENTIALS=
-# SERVER=
-# WORKSPACE=${SERVER}/workspaces/firestarr
-# STORE=$WORKSPACE/coveragestores/${LAYER}
+DIR=`dirname $(realpath "$0")`
+. ${DIR}/geoserver_setup.sh
 
-# # get rid of old granules
-# curl -v -v -sS -u "${CREDENTIALS}" -XDELETE "${STORE}/coverages/${COVERAGE}/index/granules.xml"
-# # update to match azure mount
-# curl -v -u "${CREDENTIALS}" -XPOST -H "Content-type: text/plain" --write-out %{http_code} -d "/data/firestarr_data/current" "${STORE}/external.${EXTENSION}"
+if [ -z "${LAYER}" ] \
+    || [ -z "${COVERAGE}" ] \
+    || [ -z "${CREDENTIALS}" ] \
+    || [ -z "${SERVER}" ] \
+    || [ -z "${WORKSPACE_NAME}" ] \
+    || [ -z "${DIR_DATA}" ] \
+    ; then
+    echo Missing required configuration so not publishing
+else
+    EXTENSION=imagemosaic
+    WORKSPACE=${SERVER}/workspaces/${WORKSPACE_NAME}
+    STORE=$WORKSPACE/coveragestores/${LAYER}
+    echo "Publishing to ${STORE}"
 
-# # # HACK: know that .zip implies uploaded properly and has runid in name
-# # RUN_ID=`ls -1 ../data/output/current_m3/*.zip | tail -n1 | sed "s/.*_\([0-9]*\)\.zip/\1/g"`
-# # get run id from name of files
-# RUN_ID=`curl -v -v -sS -u "${CREDENTIALS}" -XGET "${STORE}/coverages/${COVERAGE}/index/granules.xml" | grep .tif | tail -n 1 | sed "s/.*firestarr_\([0-9]*\)_.*\.tif.*/\1/g"`
-# ABSTRACT=FireSTARR run from ${RUN_ID}
-# # replace abstract
-# curl -v -v -sS -u "${CREDENTIALS}" -XGET "${STORE}/coverages/${COVERAGE}" | sed "s/<abstract>[^<]*<\/abstract>/<abstract>${ABSTRACT}<\/abstract>/g" > ${COVERAGE}.xml
-# # upload with updated abstract
-# curl -v -u "${CREDENTIALS}" -XPUT -H "Content-type: text/xml" -d @${COVERAGE}.xml "${STORE}/coverages/${COVERAGE}"
+    # # get rid of old granules
+    # curl -v -v -sS -u "${CREDENTIALS}" -XDELETE "${STORE}/coverages/${COVERAGE}/index/granules.xml"
+    # # update to match azure mount
+    # curl -v -u "${CREDENTIALS}" -XPOST -H "Content-type: text/plain" --write-out %{http_code} -d "${DIR_DATA}" "${STORE}/external.${EXTENSION}"
+
+    # # get run id from name of files
+    # RUN_ID=`curl -v -v -sS -u "${CREDENTIALS}" -XGET "${STORE}/coverages/${COVERAGE}/index/granules.xml" | grep .tif | tail -n 1 | sed "s/.*firestarr_\([0-9]*\)_.*\.tif.*/\1/g"`
+    # ABSTRACT="FireSTARR run from ${RUN_ID}"
+    # # replace abstract
+    # curl -v -v -sS -u "${CREDENTIALS}" -XGET "${STORE}/coverages/${COVERAGE}" | sed "s/<abstract>[^<]*<\/abstract>/<abstract>${ABSTRACT}<\/abstract>/g" > /tmp/${COVERAGE}.xml
+    # # upload with updated abstract
+    # curl -v -u "${CREDENTIALS}" -XPUT -H "Content-type: text/xml" -d @/tmp/${COVERAGE}.xml "${STORE}/coverages/${COVERAGE}"?calculate=nativebbox,latlonbbox,dimensions
+fi
