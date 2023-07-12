01c1bbe0144e49662330fde87839766f808dde0a
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-07-12 06:25:59 -0400

    remove showing filename from when trying to debug

--- tbd/scripts/calc_time.sh
+++ tbd/scripts/calc_time.sh
@@ -2,7 +2,6 @@
 FILE=$*
 MATCH=".*\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]\).*"
 REGEX="s/${MATCH}/\1/g"
-echo $FILE
 # get a few lines at either end in case first/last don't have timestamp
 START=`head -n10 ${FILE} | sed -n "/${MATCH}/p;" | head -n1 | sed "${REGEX}"`
 END=`tail -n10 ${FILE} | sed -n "/${MATCH}/p;" | tail -n1 | sed "${REGEX}"`
