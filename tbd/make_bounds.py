f0693b786fe4d1b43065fd3c918cf6ca5fda9b3d
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-07-17 09:51:29 -0400

    move make_bounds() code under check for main so it can be imported

--- tbd/make_bounds.py
+++ tbd/make_bounds.py
@@ -39,8 +39,8 @@ def buffer(df, km, resolution=BUFFER_RESOLUTION):
     return df
 
 
-def dissolve(df):
-    return df.dissolve(by='ID').reset_index().sort_values('EN')
+def dissolve(df, by='ID'):
+    return df.dissolve(by=by).reset_index().sort_values('EN')
 
 
 def explode(df):
@@ -78,40 +78,41 @@ def dist(c1, c2):
     assert c1.keys() == c2.keys()
     return np.sum([v.distance(c2[k]) for k, v in c1.items()])
 
-df_canada = gpd.read_file('../data/tmp/canada/lpr_000b16a_e.shp').sort_values(['PRENAME'])
-CRS_ORIG = df_canada.crs
-centroids_canada = centroids(df_canada.set_index(['PRENAME']))
+if "__main__" == __name__:
+    df_canada = gpd.read_file('../data/tmp/canada/lpr_000b16a_e.shp').sort_values(['PRENAME'])
+    CRS_ORIG = df_canada.crs
+    centroids_canada = centroids(df_canada.set_index(['PRENAME']))
 
-df_bounds = to_file(gpd.read_file('bounds.geojson').sort_values(['EN']).to_crs(CRS_ORIG), "df_bounds")
-centroids_orig = centroids(df_bounds.set_index(['EN']))
+    df_bounds = to_file(gpd.read_file('bounds.geojson').sort_values(['EN']).to_crs(CRS_ORIG), "df_bounds")
+    centroids_orig = centroids(df_bounds.set_index(['EN']))
 
-df_bounds_exact = gpd.GeoDataFrame(pd.merge(df_bounds[[x for x in df_bounds.columns if x != 'geometry']],
-                                            df_canada[['PRENAME', 'geometry']],
-                                            left_on=['EN'], right_on=['PRENAME']),
-                                    crs=CRS_ORIG)
-centroids_exact = centroids(df_bounds_exact.set_index(['EN']))
-assert centroids_exact == centroids_canada
+    df_bounds_exact = gpd.GeoDataFrame(pd.merge(df_bounds[[x for x in df_bounds.columns if x != 'geometry']],
+                                                df_canada[['PRENAME', 'geometry']],
+                                                left_on=['EN'], right_on=['PRENAME']),
+                                        crs=CRS_ORIG)
+    centroids_exact = centroids(df_bounds_exact.set_index(['EN']))
+    assert centroids_exact == centroids_canada
 
-df = to_file(df_bounds_exact, "df_bounds_exact")
+    df = to_file(df_bounds_exact, "df_bounds_exact")
 
-df = to_file(explode(df), "df_explode")
+    df = to_file(explode(df), "df_explode")
 
-df = to_file(simplify(df, 1), "df_simplify")
+    df = to_file(simplify(df, 1), "df_simplify")
 
-df = to_file(buffer(df, 100), "df_buffer")
+    df = to_file(buffer(df, 100), "df_buffer")
 
-# df = to_file(simplify(df, 10), "df_buffer_simplify")
+    # df = to_file(simplify(df, 10), "df_buffer_simplify")
 
-df = to_file(dissolve(df), "df_buffer_simplify_dissolve")
+    df = to_file(dissolve(df), "df_buffer_simplify_dissolve")
 
-df = to_file(fill(df), "df_buffer_simplify_dissolve_fill")
+    df = to_file(fill(df), "df_buffer_simplify_dissolve_fill")
 
-df = to_file(simplify(df, 10), "df_simplify_10km")
+    df = to_file(simplify(df, 10), "df_simplify_10km")
 
-df = to_file(simplify(df, 100), "df_simplify_100km")
+    df = to_file(simplify(df, 100), "df_simplify_100km")
 
-assert list(df['EN']) == list(df_bounds['EN'])
+    assert list(df['EN']) == list(df_bounds['EN'])
 
-bounds = to_file(df, 'bounds')
+    bounds = to_file(df, 'bounds')
 
-bounds.reset_index()[['ID', 'EN', 'FR', 'PRIORITY', 'DURATION', 'geometry']].set_index(['ID']).to_crs(CRS_WGS84).to_file('bounds.geojson')
+    bounds.reset_index()[['ID', 'EN', 'FR', 'PRIORITY', 'DURATION', 'geometry']].set_index(['ID']).to_crs(CRS_WGS84).to_file('bounds.geojson')
