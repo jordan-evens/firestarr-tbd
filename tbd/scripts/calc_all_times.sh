07d4575ffa7d08074a7b4a8c5e8a8fd13b0a8ce8
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-07-26 09:08:51 -0400

    use files from completed runs and processes for running simulation times

--- tbd/scripts/calc_all_times.sh
+++ tbd/scripts/calc_all_times.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR=`dirname $(realpath "$0")`
-# find -type f -name log.txt | xargs -I {} grep -il cancelled {} | xargs -I {} ${DIR}/calc_time.sh {}
-find -type f -name log.txt | sort | xargs -I {} ${DIR}/calc_time.sh {}
+# use file for completed runs and running processes instead of files for everything
+${DIR}/calc_done_times.sh | cat - <(${DIR}/calc_running_times.sh)
