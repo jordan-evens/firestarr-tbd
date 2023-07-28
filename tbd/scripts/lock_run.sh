0a68e4b1f217fbed3c0c39a042f6f6607566a815
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-07-28 18:15:14 -0400

    change to use /appl/data/update.lock as lockfile so that different containers see the lock if one is using it

--- tbd/scripts/lock_run.sh
+++ tbd/scripts/lock_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/flock -n /tmp/update.lockfile python /appl/tbd/src/py/firestarr/main.py $*
+/usr/bin/flock -n /appl/data/update.lock python /appl/tbd/src/py/firestarr/main.py $*
