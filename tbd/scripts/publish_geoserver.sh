234860f0b1e59f42ca3719d840e98c7d158137f7
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-07-28 07:08:48 -0400

    add abstract tag if it's missing instead of only replacing if it exists

--- tbd/scripts/publish_geoserver.sh
+++ tbd/scripts/publish_geoserver.sh
@@ -14,6 +14,8 @@ else
     GEOSERVER_EXTENSION=imagemosaic
     GEOSERVER_WORKSPACE=${GEOSERVER_SERVER}/workspaces/${GEOSERVER_WORKSPACE_NAME}
     GEOSERVER_STORE=${GEOSERVER_WORKSPACE}/coveragestores/${GEOSERVER_LAYER}
+    TMP_COVERAGE=/tmp/${GEOSERVER_COVERAGE}.xml
+    TAG=abstract
     echo "Publishing to ${GEOSERVER_STORE}"
 
     # get rid of old granules
@@ -24,12 +26,15 @@ else
     # get run id from name of files
     RUN_ID=`curl -v -v -sS -u "${GEOSERVER_CREDENTIALS}" -XGET "${GEOSERVER_STORE}/coverages/${GEOSERVER_COVERAGE}/index/granules.xml" | grep .tif | tail -n 1 | sed "s/.*firestarr_\([0-9]*\)_.*\.tif.*/\1/g"`
     ABSTRACT="FireSTARR run from ${RUN_ID}"
-    # replace abstract
-    curl -v -v -sS -u "${GEOSERVER_CREDENTIALS}" -XGET "${GEOSERVER_STORE}/coverages/${GEOSERVER_COVERAGE}" | sed "s/<abstract>[^<]*<\/abstract>/<abstract>${ABSTRACT}<\/abstract>/g" > /tmp/${GEOSERVER_COVERAGE}.xml
-    # upload with updated abstract
-    curl -v -u "${GEOSERVER_CREDENTIALS}" -XPUT -H "Content-type: text/xml" -d @/tmp/${GEOSERVER_COVERAGE}.xml "${GEOSERVER_STORE}/coverages/${GEOSERVER_COVERAGE}"?calculate=nativebbox,latlonbbox,dimensions
+    # replace tag
+    curl -v -v -sS -u "${GEOSERVER_CREDENTIALS}" -XGET "${GEOSERVER_STORE}/coverages/${GEOSERVER_COVERAGE}" > ${TMP_COVERAGE}
+    TAG_UPDATED="<${TAG}>${ABSTRACT}<\/${TAG}>"
+    # if no tag then insert it after title
+    (grep "<${TAG}>" ${TMP_COVERAGE} > /dev/null && sed -i "s/<${TAG}>[^<]*<\/${TAG}>/${TAG_UPDATED}/g" ${TMP_COVERAGE}) || sed -i "s/\( *\)\(<title>.*\)/\1\2\n\1${TAG_UPDATED}/g" ${TMP_COVERAGE}
+    # upload with updated tag
+    curl -v -u "${GEOSERVER_CREDENTIALS}" -XPUT -H "Content-type: text/xml" -d @${TMP_COVERAGE} "${GEOSERVER_STORE}/coverages/${GEOSERVER_COVERAGE}"?calculate=nativebbox,latlonbbox,dimensions
     # not sure why this isn't picking up .tif band description
-    sed -i "s/GRAY_INDEX/probability/g" /tmp/${GEOSERVER_COVERAGE}.xml
+    sed -i "s/GRAY_INDEX/probability/g" ${TMP_COVERAGE}
     # HACK: calculate sets band name to GRAY_INDEX so set again without calculate
-    curl -v -u "${GEOSERVER_CREDENTIALS}" -XPUT -H "Content-type: text/xml" -d @/tmp/${GEOSERVER_COVERAGE}.xml "${GEOSERVER_STORE}/coverages/${GEOSERVER_COVERAGE}"
+    curl -v -u "${GEOSERVER_CREDENTIALS}" -XPUT -H "Content-type: text/xml" -d @${TMP_COVERAGE} "${GEOSERVER_STORE}/coverages/${GEOSERVER_COVERAGE}"
 fi
