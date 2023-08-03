6f6e7a3fea4ecda1230ccdbd29879448fab3cfd8
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-08-03 12:10:55 -0400

    change to just install cmake instead of building it

--- tbd/scripts/profile.sh
+++ tbd/scripts/profile.sh
@@ -7,8 +7,8 @@ DIR_FG=/appl/FlameGraph
 # don't make this an actual submodule because it's just this script that needs it
 [ ! -d "${DIR_FG}" ] && sudo git clone https://github.com/brendangregg/FlameGraph.git ${DIR_FG}
 rm -rf ${DIR_BUILD}
-/usr/local/bin/cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -S/appl/tbd -B${DIR_BUILD} -G "Unix Makefiles"
-sudo /usr/local/bin/cmake --build ${DIR_BUILD} --config Debug --target all -j $(nproc) --
+cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -S/appl/tbd -B${DIR_BUILD} -G "Unix Makefiles"
+cmake --build ${DIR_BUILD} --config Debug --target all -j $(nproc) --
 sudo perf record -o ${FILE_PERF} -F ${FREQ} -g --call-graph=dwarf -- $* > ${DIR_PERF}/run.log
 sudo chown user:user ${FILE_PERF}
 perf script -i ${FILE_PERF} | ${DIR_FG}/stackcollapse-perf.pl | ${DIR_FG}/flamegraph.pl > flame.html
