5edaac4176f69dc029738d513eeb9b32489183fb
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-08-06 15:18:58 -0400

    fix grep so running processes are found when they use relative paths

--- tbd/scripts/calc_running_times.sh
+++ tbd/scripts/calc_running_times.sh
@@ -3,11 +3,8 @@
 # instead of looking at logs, look at process times
 # 1     get all processes
 # 2     match name of simulation process
-# 3     match simulation directory
-# 4     don't match other grep processes
-# 5     get start time column
-# 6     convert start time to seconds
-# 7     subtract seconds from current time
-# SIM_DIR=$(pwd | sed "s/.*\/\(.*\)/\1/g")
-SIM_DIR=data/sims
-ps aux | grep tbd | grep ${SIM_DIR} | grep -v grep | awk '{print $9}' | xargs -I {} date --date="{}" '+%s' | xargs -I time expr $(date '+%s') - time
+# 3     don't match other grep processes
+# 4     get start time column
+# 5     convert start time to seconds
+# 6     subtract seconds from current time
+ps aux | grep /appl/tbd/tbd | grep -v grep | awk '{print $9}' | xargs -I {} date --date="{}" '+%s' | xargs -I time expr $(date '+%s') - time
