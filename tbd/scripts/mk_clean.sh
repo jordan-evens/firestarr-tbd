04882d4d05bd63d7087891356f254cfc56ef7b4c
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-08-20 08:15:21 -0400

    added script to make clean build

---
+++ tbd/scripts/mk_clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# didn't figure out how to do this with cmake yet but this works for now
+DIR_BUILD=/appl/tbd/build
+VARIANT="$*"
+if [ -z "${VARIANT}" ]; then
+  VARIANT=Release
+fi
+echo Set VARIANT=${VARIANT}
+rm -rf ${DIR_BUILD} \
+  && /usr/bin/cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=${VARIANT} -S/appl/tbd -B${DIR_BUILD} -G "Unix Makefiles" \
+  && /usr/bin/cmake --build ${DIR_BUILD} --config ${VARIANT} --target all -j 50 --
