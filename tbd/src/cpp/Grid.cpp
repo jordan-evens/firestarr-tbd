a019eac6d7ed3c64b093d40858391255f1470a89
Shreeram Senthivasan <30534925+shreeramsenthi@users.noreply.github.com>
2024-01-18 10:55:52 -0500

    Handle forced ignitions are right and upper border

--- tbd/src/cpp/Grid.cpp
+++ tbd/src/cpp/Grid.cpp
@@ -149,8 +149,16 @@ unique_ptr<FullCoordinates> GridBase::findFullCoordinates(const topo::Point& poi
   auto actual_y = (!flipped)
                   ? (y - this->yllcorner_) / this->cell_size_
                   : (yurcorner_ - y) / cell_size_;
-  const auto column = (sim::Settings::rowColIgnition() ? static_cast<FullIdx>(sim::Settings::ignCol()) : static_cast<FullIdx>(actual_x));
-  const auto row = (sim::Settings::rowColIgnition() ? static_cast<FullIdx>(sim::Settings::ignRow()) : static_cast<FullIdx>(round(actual_y - 0.5)));
+  // Override coordinates if provided
+  if (sim::Settings::rowColIgnition())
+  {
+    return make_unique<FullCoordinates>(static_cast<FullIdx>(sim::Settings::ignRow()),
+                                        static_cast<FullIdx>(sim::Settings::ignCol()),
+                                        static_cast<SubSize>(0),
+                                        static_cast<SubSize>(0));
+  }
+  const auto column = static_cast<FullIdx>(actual_x);
+  const auto row = static_cast<FullIdx>(round(actual_y - 0.5));
 
   if (0 > column || column >= calculateColumns() || 0 > row || row >= calculateRows())
   {
