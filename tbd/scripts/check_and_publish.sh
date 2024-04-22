29df253a22968aa015f7e963a8977a7c49f0fd9c
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-04-22 15:04:22 -0400

    fix check_and_publish.sh not running from correct directory when called in crontab

--- tbd/scripts/check_and_publish.sh
+++ tbd/scripts/check_and_publish.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+DIR=/appl/tbd
+pushd ${DIR}
 echo `date -u --rfc-3339=seconds`: Running check and publish
 . /appl/data/config || . /appl/config
 source /appl/.venv/bin/activate || echo No venv
 # HACK: use python3 and not python so killall doesn't affect this
-python3 /appl/tbd/src/py/firestarr/check_and_publish.py $* || (echo FAILED)
+python3 ${DIR}/src/py/firestarr/check_and_publish.py $* || (echo FAILED)
 echo `date -u --rfc-3339=seconds`: Done running check and publish
+popd
