c3c24fa3eb90dbb7e0f034c7c95e449299ac3268
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-04-23 12:34:57 -0400

    mount /etc/ssl/certs when running 'gh act' so ssl works

--- test_gh_actions.sh
+++ test_gh_actions.sh
@@ -1,3 +1,7 @@
 USER=$(gh api user | jq -r ".login")
-gh act -a ${USER} -s GHCR_TOKEN=${GHCR_TOKEN} -j build-test-publish $*
-gh act -a ${USER} -s GITHUB_TOKEN=${GITHUB_TOKEN} -j doxygen $*
+run_job() {
+    # need to bind /etc/ssl/certs so ssl doesn't fail
+    gh act -a ${USER} --container-options "-v /etc/ssl/certs:/etc/ssl/certs:ro" -s GHCR_TOKEN=${GHCR_TOKEN} -j $*
+}
+run_job build-test-publish $*
+run_job doxygen $*
