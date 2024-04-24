7c1b7263885b2c3100a8e75714c2445d664f028d
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2024-04-24 08:21:46 -0400

    updated to use SPDX-License-Identifier instead of AGPL preamble in each file

--- tbd/src/cpp/ConvexHull.cpp
+++ tbd/src/cpp/ConvexHull.cpp
@@ -1,17 +1,7 @@
-// Copyright (c) 2005-2022, Jordan Evens
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/* Copyright (c) Jordan Evens, 2005, 2021 */
+/* Copyright (c) Queen's Printer for Ontario, 2020. */
+
+/* SPDX-License-Identifier: AGPL-3.0-or-later */
 
 #include "ConvexHull.h"
 #include <numbers>
--- tbd/src/cpp/debug_settings.cpp
+++ tbd/src/cpp/debug_settings.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020,  Queen's Printer for Ontario */
+/* Copyright (c) Queen's Printer for Ontario, 2020. */
 
 /* SPDX-License-Identifier: AGPL-3.0-or-later */
 
