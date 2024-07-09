CWFMF/firestarr-cpp

use another transform_reduce() call

src/cpp/MergeIterator.h
@@ -21,6 +21,72 @@ using map_type = map<Location, vector<InnerPos>>;
 const merged_map_type::mapped_type
 merge_cell_data(const merged_map_type::mapped_type& lhs, const merged_map_type::mapped_type& rhs);
 
+template <typename M, class F>
+const M
+merge_maps_generic(
+  const M& lhs,
+  const M& rhs,
+  F f
+)
+{
+  using value_type = typename M::value_type;
+  using key_type = typename M::key_type;
+  using mapped_type = typename M::mapped_type;
+  std::function<mapped_type(const mapped_type&, const mapped_type&)> fct_merge = f;
+  M out{};
+  auto it_lhs = lhs.begin();
+  auto it_rhs = rhs.begin();
+  auto end_lhs = lhs.end();
+  auto end_rhs = rhs.end();
+  while (true)
+  {
+    if (end_lhs == it_lhs)
+    {
+      while (end_rhs != it_rhs)
+      {
+        out.emplace(*it_rhs);
+        ++it_rhs;
+      }
+      break;
+    }
+    if (end_rhs == it_rhs)
+    {
+      while (end_lhs != it_lhs)
+      {
+        out.emplace(*it_lhs);
+        ++it_lhs;
+      }
+      break;
+    }
+    // at end of neither so pick lower value
+    const value_type& pair0 = *it_lhs;
+    const value_type& pair1 = *it_rhs;
+    const key_type& k0 = pair0.first;
+    const key_type& k1 = pair1.first;
+    const mapped_type& m0 = pair0.second;
+    const mapped_type& m1 = pair1.second;
+    if (k0 < k1)
+    {
+      out.emplace(pair0);
+      ++it_lhs;
+    }
+    else if (k0 > k1)
+    {
+      out.emplace(pair1);
+      ++it_rhs;
+    }
+    else
+    {
+      assert(k0 == k1);
+      const mapped_type merged = fct_merge(m0, m1);
+      out.emplace(pair<const key_type, const mapped_type>(k0, merged));
+      ++it_lhs;
+      ++it_rhs;
+    }
+  }
+  return out;
+}
+
 const merged_map_type
 merge_maps(const merged_map_type& lhs, const merged_map_type& rhs);
 

jordan-evens/firestarr-tbd

tbd/src/cpp/MergeIterator.h
@@ -20,6 +20,71 @@ const merged_map_type::mapped_type merge_cell_data(
   const merged_map_type::mapped_type& lhs,
   const merged_map_type::mapped_type& rhs);
 
+template <typename M, class F>
+const M merge_maps_generic(
+  const M& lhs,
+  const M& rhs,
+  F f)
+{
+  using const_iterator = typename M::const_iterator;
+  using value_type = typename M::value_type;
+  using key_type = typename M::key_type;
+  using mapped_type = typename M::mapped_type;
+  std::function<mapped_type(const mapped_type&, const mapped_type&)> fct_merge = f;
+  M out{};
+  const_iterator it_lhs = lhs.begin();
+  const_iterator it_rhs = rhs.begin();
+  const_iterator end_lhs = lhs.end();
+  const_iterator end_rhs = rhs.end();
+  while (true)
+  {
+    if (end_lhs == it_lhs)
+    {
+      while (end_rhs != it_rhs)
+      {
+        out.emplace(*it_rhs);
+        ++it_rhs;
+      }
+      break;
+    }
+    if (end_rhs == it_rhs)
+    {
+      while (end_lhs != it_lhs)
+      {
+        out.emplace(*it_lhs);
+        ++it_lhs;
+      }
+      break;
+    }
+    // at end of neither so pick lower value
+    const value_type& pair0 = *it_lhs;
+    const value_type& pair1 = *it_rhs;
+    const key_type& k0 = pair0.first;
+    const key_type& k1 = pair1.first;
+    const mapped_type& m0 = pair0.second;
+    const mapped_type& m1 = pair1.second;
+    if (k0 < k1)
+    {
+      out.emplace(pair0);
+      ++it_lhs;
+    }
+    else if (k0 > k1)
+    {
+      out.emplace(pair1);
+      ++it_rhs;
+    }
+    else
+    {
+      assert(k0 == k1);
+      const mapped_type merged = fct_merge(m0, m1);
+      out.emplace(pair<const key_type, const mapped_type>(k0, merged));
+      ++it_lhs;
+      ++it_rhs;
+    }
+  }
+  return out;
+}
+
 const merged_map_type merge_maps(
   const merged_map_type& lhs,
   const merged_map_type& rhs);