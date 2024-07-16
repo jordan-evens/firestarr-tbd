32dbde62600aab9cf10a0358ce1ff1bfa22f3c93
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-07-16 07:51:58 -0600

    0.8.9.13 fix archive_sims.sh not working if corrupt archive in sims.bkup folder

--- tbd/scripts/archive_sims.sh
+++ tbd/scripts/archive_sims.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
-
 KEEP=10
 
-pushd /appl/data/sims
-mkdir -p ../sims.bkup
+DIR_FROM="/appl/data/sims"
+DIR_BKUP="/appl/data/sims.bkup"
+
+function do_archive()
+{
+  run="$1"
+  file_out="${DIR_BKUP}/${run}.7z"
+  echo "Archiving ${run}"
+  if [ -f "${file_out}" ]; then
+    echo "Checking existing archive ${file_out}"
+    # if 7z can't open the archive then we need to get rid of it
+    7za t "${file_out}" || (rm "${file_out}")
+  fi
+  7za a -mx=9 -r -sdel "${file_out}" "${DIR_FROM}/${run}/*" && rmdir "${DIR_FROM}/${run}"
+}
+
+pushd ${DIR_FROM}
+mkdir -p ${DIR_BKUP}
 rmdir * > /dev/null 2>&1
-ls -1 | head -n-${KEEP} | xargs -I {} bash -c "7za a -mx=9 -r -sdel ../sims.bkup/{}.7z {}/* && rmdir {}"
+set -e
+for run in `ls -1 | head -n-${KEEP}`
+do
+  do_archive "${run}"
+done
+
 popd
