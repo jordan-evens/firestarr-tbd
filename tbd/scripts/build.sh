5f48e3cf52588944e2d634b17a3df1a325539a1b
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-07-25 13:48:01 -0400

    add scripts/build.sh to rebuild without cleaning

---
+++ tbd/scripts/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# didn't figure out how to do this with cmake yet but this works for now
+DIR_BUILD=/appl/tbd/build
+VARIANT="$*"
+if [ -z "${VARIANT}" ]; then
+  VARIANT=Release
+fi
+echo Set VARIANT=${VARIANT}
+/usr/bin/cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=${VARIANT} -S/appl/tbd -B${DIR_BUILD} -G "Unix Makefiles" \
+  && /usr/bin/cmake --build ${DIR_BUILD} --config ${VARIANT} --target all -j 50 --
