abc51ce4404d29a4a580b985acf492f855190b12
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-07-26 10:54:49 -0400

    fix lock_ scripts not passing arguments

--- tbd/scripts/lock_merge.sh
+++ tbd/scripts/lock_merge.sh
@@ -3,4 +3,4 @@ DIR=`dirname $(realpath "$0")`
 export FORCE_RUN=${FORCE_RUN}
 export IS_CRONJOB=${IS_CRONJOB}
 
-${DIR}/with_lock_update.sh ${DIR}/with_lock_publish.sh /appl/tbd/scripts/merge_inputs.sh
+${DIR}/with_lock_update.sh ${DIR}/with_lock_publish.sh /appl/tbd/scripts/merge_inputs.sh $*
--- tbd/scripts/lock_publish.sh
+++ tbd/scripts/lock_publish.sh
@@ -3,4 +3,4 @@ DIR=`dirname $(realpath "$0")`
 export FORCE_RUN=${FORCE_RUN}
 export IS_CRONJOB=${IS_CRONJOB}
 
-${DIR}/with_lock_publish.sh /appl/tbd/scripts/check_and_publish.sh
+${DIR}/with_lock_publish.sh /appl/tbd/scripts/check_and_publish.sh $*
