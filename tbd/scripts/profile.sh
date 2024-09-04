0dd429709ef17e644984f514d96592022c6f5cac
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-09-04 07:37:26 -0400

    fixed chown using '-r' instead of '-R'

--- tbd/scripts/profile.sh
+++ tbd/scripts/profile.sh
@@ -6,7 +6,7 @@ DIR_FG=/appl/FlameGraph
 sudo sysctl -w kernel.perf_event_paranoid=1
 [ ! -d "${DIR_PERF}" ] && mkdir -p "${DIR_PERF}"
 # don't make this an actual submodule because it's just this script that needs it
-[ ! -d "${DIR_FG}" ] && sudo git clone https://github.com/brendangregg/FlameGraph.git ${DIR_FG} && sudo chown -r ${USER}:${USER} ${DIR_FG}
+[ ! -d "${DIR_FG}" ] && sudo git clone https://github.com/brendangregg/FlameGraph.git ${DIR_FG} && sudo chown -R ${USER}:${USER} ${DIR_FG}
 rm -rf ${DIR_BUILD}
 cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -S/appl/tbd -B${DIR_BUILD} -G "Unix Makefiles"
 cmake --build ${DIR_BUILD} --config Debug --target all -j $(nproc) --
