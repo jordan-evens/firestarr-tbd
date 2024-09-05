834f28031aff2f4a5ad62ba1753f13ad9516e2e4
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-09-05 11:11:46 -0400

    fix profile.sh not copying binary to /appl/tbd/tbd so scripts called by it used whatever happened to be there

--- tbd/scripts/profile.sh
+++ tbd/scripts/profile.sh
@@ -10,6 +10,7 @@ sudo sysctl -w kernel.perf_event_paranoid=1
 rm -rf ${DIR_BUILD}
 cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -S/appl/tbd -B${DIR_BUILD} -G "Unix Makefiles"
 cmake --build ${DIR_BUILD} --config Debug --target all -j $(nproc) --
+cp ${DIR_BUILD}/tbd /appl/tbd/tbd
 time perf record -o ${FILE_PERF} -F ${FREQ} -g --call-graph=dwarf -- $* > ${DIR_PERF}/run.log
 chown ${USER}:${USER} ${FILE_PERF}
 perf script -i ${FILE_PERF} | ${DIR_FG}/stackcollapse-perf.pl | ${DIR_FG}/flamegraph.pl > flame.html
