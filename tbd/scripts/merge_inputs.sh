772c078397ea10b2a604fc1d0bcddb7141aae073
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-09-17 15:32:31 -0400

    WIP: copy old outputs to new folders when merging so results are still there when publishing before sims run

--- tbd/scripts/merge_inputs.sh
+++ tbd/scripts/merge_inputs.sh
@@ -13,34 +13,26 @@ set -e
 
 echo "Checking that previous run completed and published successfully"
 # NOTE: don't use --resume because if it's old weather it needs to run from scratch
-(${DIR}/update.sh --no-publish --no-merge --no-retry && ${DIR}/check_and_publish.sh) || (echo "Previous run didn't finish properly" && exit -1)
+(${DIR}/update.sh --no-publish --no-merge --no-retry --resume && ${DIR}/check_and_publish.sh)
+RESULT=$?
+if [ 0 -ne "${RESULT}" ]; then
+ echo "Previous run didn't finish properly"
+ exit ${RESULT}
+fi
 
 LAST_RUN=`ls -1 ${DIR_SIMS} | sort | grep -v "${SUBDIR_COMMON}" | tail -n 1`
 dir_last_sims="${DIR_SIMS}/${LAST_RUN}"
 dir_last_runs="${DIR_RUNS}/${LAST_RUN}"
 
-# couldn't we just point the whole "current" directory at the latest run?
-# # use symlinks to last run if folders for "current" don't exist yet
-# if [ ! -d "${DIR_RUNS}/${SUBDIR_COMMON}" ]; then
-#     mkdir -p "${DIR_RUNS}/${SUBDIR_COMMON}"
-#     ls -1 ${dir_last_runs} | grep -v ".log" | xargs -tI {} ln -s "${dir_last_runs}/{}" "${DIR_RUNS}/${SUBDIR_COMMON}/{}"
-# fi
-# if [ ! -d "${DIR_SIMS}/${SUBDIR_COMMON}" ]; then
-#     # should point at all folders in last run
-#     mkdir -p "${DIR_SIMS}/${SUBDIR_COMMON}"
-#     ls -1 ${dir_last_sims} | xargs -tI {} ln -s "${dir_last_sims}/{}" "${DIR_SIMS}/${SUBDIR_COMMON}/{}"
-# fi
-
-
-# # if we could get it running
-# LAST_RUN=`ls -1 ${DIR_SIMS} | sort | tail -n 1`
-# # need to cancel this before it runs things
-# scripts/lock_run.sh --no-publish --no-merge --no-resume --no-retry
-# # at this point expect to have all the sims prepared but nothing running
-
 # since previous run finished fine we can do this
 echo "Preparing new run to merge into"
-(${DIR}/update.sh --prepare-only --no-resume --no-retry) || (echo "Couldn't create new run to merge into" && exit -1)
+(${DIR}/update.sh --prepare-only --no-resume --no-retry)
+# do this instead of || so we can copy the whole line above when debugging
+RESULT=$?
+if [ 0 -ne "${RESULT}" ]; then
+ echo "Couldn't create new run to merge into"
+ exit ${RESULT}
+fi
 
 # same as above since we just added another directory to the end of the list
 CUR_RUN=`ls -1 ${DIR_SIMS} | sort | grep -v "${SUBDIR_COMMON}" | tail -n 1`
@@ -65,19 +57,37 @@ for d in `diff -q ${dir_last_sims} ${dir_cur_sims} | grep -v "model" | grep "Com
     else
         # check that sim conditions, input tif and weather are the same
         # only care about simulation call line being the same so we can tweak script otherwise and still see as the same
-        diff <(grep tbd ${old}/sim.sh) <(grep tbd ${new}/sim.sh) > /dev/null || continue
-        (diff -rqs ${old} ${new} | grep -v ".lock" | grep identical | grep ${d}/${d}.tif) > /dev/null || continue
-        (diff -rqs ${old} ${new} | grep -v ".lock" | grep identical | grep ${d}/firestarr_${d}_wx.csv) > /dev/null || continue
-        echo "Results for ${d} are from same inputs so reusing"
-        # instead of copying just use symlinks
-        # NOTE: could use links that point forward in time so when things get archived links are still valid
-        #       but if we keep all the archives for today at least then we should be able to point backward
-        #       to avoid moving and copying things so much
-        # # move previous results into this run
-        # rm -rf ${new} && mv ${old} ${new} && ln -s ${new} ${old}
-        # symlink to old run from new
-        # use relative symlink so path should be good in azure still
-        rm -rf ${new} && ln -s `realpath ${old} --relative-to=${dir_cur_sims}` ${new}
+        # not sure how to chain these without continue
+        (diff <(grep tbd ${old}/sim.sh) <(grep tbd ${new}/sim.sh) > /dev/null) \
+            && ((diff -rqs ${old} ${new} | grep -v ".lock" | grep identical | grep ${d}/${d}.tif) > /dev/null) \
+            && ((diff -rqs ${old} ${new} | grep -v ".lock" | grep identical | grep ${d}/firestarr_${d}_wx.csv) > /dev/null)
+        # if last return succeeded then no differences
+        if [ "0" -eq "$?" ]; then
+            echo "Results for ${d} are from same inputs so reusing"
+            # instead of copying just use symlinks
+            # NOTE: could use links that point forward in time so when things get archived links are still valid
+            #       but if we keep all the archives for today at least then we should be able to point backward
+            #       to avoid moving and copying things so much
+            # # move previous results into this run
+            # rm -rf ${new} && mv ${old} ${new} && ln -s ${new} ${old}
+            # symlink to old run from new
+            # use relative symlink so path should be good in azure still
+            # rm -rf ${new} && ln -s `realpath ${old} --relative-to=${dir_cur_sims}` ${new}
+        else
+            # check if files already exist in new directory (somehow - maybe ran already in interim?)
+            ls -1 ${new}/*probability*.tif > /dev/null 2>&1
+            if [ "0" -eq "$?" ]; then
+                echo "Rasters already exist in new directory so not copying"
+            else
+                # something has changed but we don't want to throw out the old results before we have new ones
+                echo "Moving old results into new folder"
+                # just worry about rasters since using these as interim results
+                for r in `ls -1 ${old}/*probability*.tif`; do
+                    r_new=`basename ${r} | sed "s/^.*probability_/interim_probability_/g"`
+                    cp -v "${r}" "${new}/${r_new}"
+                done
+            fi
+        fi
     fi
     copied=$(expr ${copied} + 1)
 done
@@ -97,9 +107,24 @@ else
     ln -sfn "`realpath ${dir_cur_sims} --relative-to=${DIR_SIMS}`" "${dir_common_sims}"
     ln -sfn "`realpath ${dir_cur_runs} --relative-to=${DIR_RUNS}`" "${dir_common_runs}"
     # if we call pointing at current directory it should make tasks there instead of using actual folder name
-    (${DIR}/update.sh --no-publish --no-merge --no-retry "${dir_common_runs}") || (echo "Merged run didn't finish properly" && exit -1)
-    # echo "Validating merged run in ${CUR_RUN}"
-    # (${DIR}/update.sh --no-publish --no-merge --no-retry) || (echo "Validating merged run" && exit -1)
-    # echo "Publishing merged run in ${CUR_RUN}"
-    # (${DIR}/check_and_publish.sh) || (echo "Couldn't publish after finished" && exit -1)
+    (${DIR}/update.sh --no-publish --no-merge --no-retry "${dir_common_runs}")
+    RESULT=$?
+    if [ 0 -ne "${RESULT}" ]; then
+        echo "Merged run didn't finish properly"
+        exit ${RESULT}
+    fi
+    echo "Validating merged run in ${CUR_RUN}"
+    (${DIR}/update.sh --no-publish --no-merge --no-retry "${dir_common_runs}")
+    RESULT=$?
+    if [ 0 -ne "${RESULT}" ]; then
+        echo "Validating merged run"
+        exit ${RESULT}
+    fi
+    echo "Publishing merged run in ${CUR_RUN}"
+    (${DIR}/check_and_publish.sh)
+    RESULT=$?
+    if [ 0 -ne "${RESULT}" ]; then
+        echo "Couldn't publish after finished"
+        exit ${RESULT}
+    fi
 fi
