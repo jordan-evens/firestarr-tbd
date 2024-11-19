bf02fec5a209ff12e0821503313624ccfa85145e
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-11-19 14:22:10 -0500

    fix archive script not running properly by using /tmp/bkup as interim merge spot for runs & sims folders

--- tbd/scripts/archive_sims.sh
+++ tbd/scripts/archive_sims.sh
@@ -5,6 +5,7 @@ KEEP_UNARCHIVED=10
 DIR_FROM_SIMS="/appl/data/sims"
 DIR_FROM_RUNS="/appl/data/runs"
 DIR_BKUP="/appl/data/sims.bkup"
+DIR_TMP="/tmp/bkup"
 SUBDIR_COMMON="current"
 
 # override KEEP_UNARCHIVED if set in config
@@ -18,43 +19,53 @@ function do_archive()
   # zip_type="u"
   # '-sdel' doesn't work with 'u'
   zip_type="a"
-  options="-mtm -mtc -mx=9 -stl"
+  # always -sdel since archiving from tmp folder
+  options="-mtm -mtc -mx=9 -stl -sdel"
   file_out="${DIR_BKUP}/${run}.7z"
   dir_sims="${DIR_FROM_SIMS}/${run}"
   dir_runs="${DIR_FROM_RUNS}/${run}"
-  if [ "" != "${do_delete}" ]; then
-    echo "Archiving and deleting ${run}"
-    options="${options} -sdel"
-  else
-    echo "Archiving without deleting ${run}"
-  fi
-  file_newest=`(find ${dir_sims} -type f -printf '%T@ %P\n'; find ${dir_runs} -type f -printf '%T@ %P\n')  | sort -n | tail -n1 | awk '{print $2}'`
+  dir_tmp="${DIR_TMP}/${run}"
+  mkdir -p "${DIR_TMP}"
   echo "Archiving ${run} as ${file_out}"
+  # if the folders still exist then don't try to figure out what files are newer
   if [ -f "${file_out}" ]; then
     echo "Updating ${file_out}"
     echo "Checking existing archive ${file_out}"
     # if 7z can't open the archive then we need to get rid of it
     7za t "${file_out}" || (echo "Removing invalid file ${file_out}" && rm "${file_out}")
-    if [ -f "${file_out}" ] && [ ! "${file_newest}" -nt "${file_out}" ]; then
-      echo "Archive already up-to-date"
-      return
-    fi
+  fi
+  # HACK: merge directories before zipping
+  if [ "" != "${do_delete}" ]; then
+    echo "Archiving and deleting ${run}"
+    # sims is the bigger folder so move it
+    # NOTE: this is on azure so really slow
+    mv -v ${dir_sims} ${dir_tmp}
+    # dir_runs copied below and deleted at end
   else
-    echo "Creating ${file_out}"
+    echo "Archiving without deleting ${run}"
+    # don't want to break things if this fails, so don't just move for now
+    rsync -avHP ${dir_sims}/ ${dir_tmp}
   fi
-  if [ -d "${dir_sims}" ]; then
-    echo "Adding ${dir_sims}"
-    7za ${zip_type} ${options} "${file_out}" "${dir_sims}/*" \
-      && rmdir --ignore-fail-on-non-empty "${dir_sims}"
-  fi
-  if [ -d "${dir_runs}" ]; then
-    echo "Adding ${dir_runs}"
-    7za ${zip_type} ${options} "${file_out}" "${dir_runs}/*" \
-      && rmdir --ignore-fail-on-non-empty "${dir_runs}"
+  rsync -avHP ${dir_runs}/ ${dir_tmp}
+
+  echo "Creating ${file_out}"
+  7za ${zip_type} ${options} "${file_out}" "${dir_tmp}/*" \
+      && rmdir "${dir_tmp}"
+  RESULT=$?
+  if [ 0 -ne "${RESULT}" ]; then
+    echo "Failed to archive ${run}"
+  else
+    if [ "" != "${do_delete}" ]; then
+      echo "Removing original folders" \
+        && rm -rf ${dir_runs} \
+        && rm -rf ${dir_sims}
+    fi
   fi
 }
 
 pushd ${DIR_FROM_RUNS}
+# get rid of bkup folder in case old junk is in there
+# rm -rf ${DIR_BKUP} && \
 mkdir -p ${DIR_BKUP}
 rmdir * > /dev/null 2>&1
 set -e
@@ -62,6 +73,7 @@ match_last=`ls -1 | grep -v "${SUBDIR_COMMON}" | tail -n1 | sed "s/.*\([0-9]\{8\
 # also filter out anything for today since might be symlinking to it
 for run in `ls -1  | grep -v "${SUBDIR_COMMON}" | grep -v "${match_last}" | head -n-${KEEP_UNARCHIVED}`
 do
+  echo "${run}"
   do_archive "${run}" 1
 done
 
