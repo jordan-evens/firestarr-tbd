96556fa804d026900608594630a9fc5e96176450
Shreeram Senthivasan <shreeramsenthi@gmail.com>
2024-11-25 12:48:45 -0500

    Adjust step sizes for widest ellipse fire spread

--- tbd/src/cpp/FireSpread.cpp
+++ tbd/src/cpp/FireSpread.cpp
@@ -351,8 +351,7 @@ SpreadInfo::SpreadInfo(const DurationSize time,
   max_intensity_ = fuel::fire_intensity(tfc_, head_ros_);
   l_b_ = fuel->lengthToBreadth(wsv);
   const HorizontalAdjustment correction_factor = horizontal_adjustment(slope_azimuth, percentSlope());
-  // const auto spread_algorithm = OriginalSpreadAlgorithm(0.5, cell_size, min_ros);
-  // const auto spread_algorithm = OriginalSpreadAlgorithm(1, cell_size, min_ros);
+  // const auto spread_algorithm = OriginalSpreadAlgorithm(1.0, cell_size, min_ros);
   const auto spread_algorithm = WidestEllipseAlgorithm(5.0, cell_size, min_ros);
   offsets_ = spread_algorithm.calculate_offsets(correction_factor,
                                                 tfc_,
--- tbd/src/cpp/SpreadAlgorithm.cpp
+++ tbd/src/cpp/SpreadAlgorithm.cpp
@@ -244,8 +244,9 @@ HorizontalAdjustment horizontal_adjustment(
   bool added = true;
 #define STEP_X 0.2
 #define STEP_MAX util::to_radians(max_angle_)
-  MathSize step_x = STEP_X;
-  // MathSize step_x = STEP_X / length_to_breadth;
+  //MathSize step_x = STEP_X;
+  //MathSize step_x = STEP_X / length_to_breadth;
+  MathSize step_x = STEP_X / pow(length_to_breadth, 0.5);
   MathSize theta = 0;
   MathSize angle = 0;
   MathSize last_theta = 0;
@@ -309,7 +310,7 @@ HorizontalAdjustment horizontal_adjustment(
     // last_angle = angle;
   }
   // just because 5 seems good for the front and 10 for the back
-  step_max = 2.0 * STEP_MAX;
+  //step_max = 2.0 * STEP_MAX;
   cur_x -= (step_x / 2.0);
   // trying to pick less rear points
   // step_x *= length_to_breadth;
