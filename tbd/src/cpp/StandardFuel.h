8ee46b618bdb6a008ce7bc43a9bab634317ead19
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-12-02 14:55:37 -0500

    change bui effect to be 1.0 when BUI is 0 as per R package

--- tbd/src/cpp/StandardFuel.h
+++ tbd/src/cpp/StandardFuel.h
@@ -194,8 +194,12 @@ public:
    */
   [[nodiscard]] MathSize buiEffect(const MathSize bui) const noexcept override
   {
-    return exp(
-      50.0 * log_q_.asValue() * ((1.0 / (0 == bui ? 1.0 : bui)) - (1.0 / bui0())));
+    return (0 < bui)
+           ? exp(
+               50.0
+               * log_q_.asValue()
+               * ((1.0 / bui) - (1.0 / bui0())))
+           : 1.0;
   }
 protected:
   ~StandardFuel() = default;
